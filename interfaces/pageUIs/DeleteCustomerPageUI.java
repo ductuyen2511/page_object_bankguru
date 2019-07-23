7c59696b243c54e8e69e71bc8e1de30f9ed92b67
Leon <Leon@DESKTOP-1227H6R>
2019-07-23 22:57:58 +0700

    update

---
+++ interfaces/pageUIs/DeleteCustomerPageUI.java
@@ -0,0 +1,6 @@
+package pageUIs;
+
+public class DeleteCustomerPageUI {
+	public static final String CUSTOMER_ID_TEXTBOX = "//input[@name = 'cusid']";
+	public static final String SUBMIT_BUTTON_DELETE_CUSTOMER = "//input[@name = 'AccSubmit']";
+}
